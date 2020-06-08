b50b7f8e095520f5b06df0c101f25a6ec3de67b2
ancherys <62359633+ancherys@users.noreply.github.com>
2020-06-08 12:51:50 +0300

    SearchUsers added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+
 import axios from 'axios';
 import './App.css';
 
@@ -9,23 +11,39 @@ class App extends Component {
 		users: [],
 		loading: false,
 	};
-	async componentDidMount() {
-		// In react we can not do this.state.loading = true
-		this.setState({ loading: true });
 
+	// Users load on load
+	// async componentDidMount() {
+	// 	// In react we can not do this.state.loading = true
+	// 	this.setState({ loading: true });
+
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITFINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITFINDER_CLIENT_SECRET}`
+	// 	);
+
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	// Search Github users (from Search.js)
+	searchUsers = async text => {
+		this.setState({ loading: true });
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITFINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITFINDER_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITFINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITFINDER_CLIENT_SECRET}`
 		);
 
-		this.setState({ users: res.data, loading: false });
-	}
+		this.setState({ users: res.data.items, loading: false });
+
+		console.log(text);
+	};
 
 	render() {
 		// Users passing as props to users.js (2 props)
 		return (
 			<div className='App'>
 				<Navbar />
+
 				<div className='container'>
+					<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users} />
 				</div>
 			</div>
