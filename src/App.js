72a879483460fad5c5a15d280f6c89979e4913db
ancherys <62359633+ancherys@users.noreply.github.com>
2020-06-09 18:51:47 +0300

    user page updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null,
 	};
@@ -35,16 +37,22 @@ class App extends Component {
 		);
 
 		this.setState({ users: res.data.items, loading: false });
+	};
 
-		console.log(text);
+	// Get single github user
+	getUser = async username => {
+		this.setState({ loading: true });
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITFINDER_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITFINDER_CLIENT_SECRET}`
+		);
+
+		this.setState({ user: res.data, loading: false });
 	};
 
 	// Clear users from state
-
 	clearUsers = () => this.setState({ users: [], loading: false });
 
 	// Set Alert
-
 	setAlert = (msg, type) => {
 		this.setState({ alert: { msg, type } });
 
@@ -53,7 +61,7 @@ class App extends Component {
 
 	render() {
 		// Destructuring
-		const { users, loading } = this.state;
+		const { users, user, loading } = this.state;
 		// Users passing as props to users.js (2 props)
 
 		return (
@@ -81,6 +89,18 @@ class App extends Component {
 								)}
 							/>
 							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user/:login'
+								render={props => (
+									<User
+										{...props}
+										getUser={this.getUser}
+										user={user}
+										loading={loading}
+									/>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
